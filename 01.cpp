8a8bb2a64c778a33d5be20029b62426da10abdee
dliszka <dliszka@crodu.com>
2017-03-25 17:32:10 +0100

    LAB M01 complete

--- 01.cpp
+++ 01.cpp
@@ -2,6 +2,34 @@
 
 using std::cout;
 
+template<typename T>
+short int getPrecision(T& number){
+	short int sum=0;
+	T EPSILON;
+	while(true){
+		number /= 2;
+		++sum;
+		if(number + 1 == 1){
+			number = EPSILON;
+			--sum;
+			break;
+		}
+		EPSILON = number;
+	}
+	return sum;
+}
+
 int main() {
+	float  _FLOAT  = 1.0;
+	double _DOUBLE = 1.0;
+
+	cout << "FLAOT HAS GOT " << getPrecision(_FLOAT) << "BITS OF MANTISSA"  << "\n";
+	cout << "EPSYLON FOR FLOAT = "<<_FLOAT << "\n";
+
+	cout << "\n";
+
+	cout << "DOUBLE HAS GOT " << getPrecision(_DOUBLE) << "BITS OF MANTISSA"  << "\n";
+	cout << "EPSYLON FOR DOUBLE = "<<_DOUBLE << "\n";
+
 	return 0;
 }
