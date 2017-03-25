e3edae94c99ab9c580825e0530ef2fbe4ca2a799
dliszka <dliszka@crodu.com>
2017-03-25 17:43:24 +0100

    Added oneminexp_ref file, and also added few lines for opening file

--- 02.cpp
+++ 02.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <cmath>
 
 using namespace std;
 
+long double getValue(long double x) {
+	return (1-exp(x))/x;
+}
+
 int main() {
+	fstream FILE;
+	FILE.open("oneminexp_ref.txt");
+
+	/*
+		READING
+	*/
+
+	FILE.close();
+
 	return 0;
 }
